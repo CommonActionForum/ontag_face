b8a20f1ee5892dacdab830a051c30b5dd97fe137
Carlos Saito <c.saito.style@gmail.com>
2017-06-28 13:23:53 +0200

    Drop props from <TagTooltip>

--- client/components/annotators/text-annotator/tag-tooltip.js
+++ client/components/annotators/text-annotator/tag-tooltip.js
@@ -12,7 +12,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Tooltip = ({ list, selected, onSelect, onUnselect, position }) => (
+const Tooltip = ({ list, onSelect, position }) => (
   <div
     className='tooltip tooltip-bottom'
     style={{
@@ -24,57 +24,43 @@ const Tooltip = ({ list, selected, onSelect, onUnselect, position }) => (
     }}
   >
     <div className="tooltip-arrow"></div>
-    {selected
-     ? (
-       <div className='tooltip-inner'>
-         <button
-           className='btn btn-link'
-           onClick={() => onUnselect()}
-         >
-           ×
-         </button>
-         {list.filter(tag => tag.id === selected)[0].title}
-       </div>
-     )
-     : (
-       <div className='tooltip-inner'>
-         <ul
-           style={{
-             padding: 0,
-             margin: 0,
-             listStyle: 'none'
-           }}
-         >
-           {
-             list.map(({ref, title}) => (
-               <li
-                 key={ref}
-                 style={{
-                   padding: '5px'
-                 }}
-               >
-                 <button
-                   style={{
-                     color: '#FFF',
-                     fontFamily: 'sans-serif',
-                     fontWeight: 'bold',
-                     padding: '5px',
-                     background: 'none',
-                     border: 0,
-                     cursor: 'pointer',
-                     outline: 'none',
-                     textAlign: 'left'
-                   }}
-                   onClick={() => onSelect(ref)}
-                 >
-                   {title}
-                 </button>
-               </li>
-             ))
-           }
-         </ul>
-       </div>
-     )}
+    <div className='tooltip-inner'>
+      <ul
+        style={{
+          padding: 0,
+          margin: 0,
+          listStyle: 'none'
+        }}
+      >
+        {
+          list.map(({ref, title}) => (
+            <li
+              key={ref}
+              style={{
+                padding: '5px'
+              }}
+            >
+              <button
+                style={{
+                  color: '#FFF',
+                  fontFamily: 'sans-serif',
+                  fontWeight: 'bold',
+                  padding: '5px',
+                  background: 'none',
+                  border: 0,
+                  cursor: 'pointer',
+                  outline: 'none',
+                  textAlign: 'left'
+                }}
+                onClick={() => onSelect(ref)}
+              >
+                {title}
+              </button>
+            </li>
+          ))
+        }
+      </ul>
+    </div>
   </div>
 )
 
@@ -88,9 +74,7 @@ Tooltip.propTypes = {
       title: PropTypes.string.isRequired
     })
   ),
-  selected: PropTypes.number,
-  onSelect: PropTypes.func.isRequired,
-  onUnselect: PropTypes.func
+  onSelect: PropTypes.func.isRequired
 }
 
 export default Tooltip
