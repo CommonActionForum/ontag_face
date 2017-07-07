9c9d94021dc930e08447a7f54970eb7bec316f27
Carlos Saito <c.saito.style@gmail.com>
2017-07-07 10:39:29 +0200

    Drop properties from <TextAnnotator />
    New TextAnnotator component only allows three properties:
    - children
    - tags
    - onCreateAnnotation

--- stories/text-annotator.js
+++ stories/text-annotator.js
@@ -6,7 +6,6 @@ import TextAnnotator from '../client/components/annotators/text-annotator/text-a
 
 storiesOf('TextAnnotator', module)
   .add('A normal usage', () => {
-    const annotations = []
     const tags = [
       { ref: 't1', title: 'Tag 1' },
       { ref: 't2', title: 'Tag 2' },
@@ -16,9 +15,7 @@ storiesOf('TextAnnotator', module)
     return (
       <TextAnnotator
         tags={tags}
-        annotations={annotations}
         onCreateAnnotation={action('create annotation')}
-        onDeleteAnnotation={action('delete annotation')}
       >
         Hola multimundo
       </TextAnnotator>
