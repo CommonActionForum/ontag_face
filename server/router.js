19505c4a6015bd09194feed70ba69795c9d59460
Carlos Saito <c.saito.style@gmail.com>
2017-09-24 20:12:11 +0200

    Log more information in endpoints and middlewares

--- server/router.js
+++ server/router.js
@@ -18,18 +18,22 @@ router.get('/parseArticle', parseArticle)
 router.get('/annotate', checkSession, annotate)
 
 router.get('/about', (req, res) => {
+  console.log('ENDPOINT /about. Rendering about')
   res.render('about')
 })
 
 router.post('/login', urlencodedParser, login, (req, res) => {
+  console.log('ENDPOINT POST /login. Redirecting to home')
   res.redirect('/')
 })
 
 router.get('/login', (req, res) => {
+  console.log('ENDPOINT GET /login. Rendering index')
   res.render('index')
 })
 
 router.get('*', (req, res) => {
+  console.log('ENDPOINT 404. ' + req.originalUrl)
   res.send('404 Not found')
 })
 
