1697111ad642a572d9c719952ed65551731b8feb
Carlos Saito <c.saito.style@gmail.com>
2017-10-04 13:50:27 +0200

    Update /annotate

--- server/endpoints/annotate.js
+++ server/endpoints/annotate.js
@@ -7,4 +7,22 @@ export default async function annotate (req, res) {
   const entryId = req.query.entry
   const questionId = req.query.question
 
+  console.log('ENDPOINT annotate > Calling core.entries.show()')
+  const p1 = req
+    .core.entries.show(entryId)
+
+  console.log('ENDPOINT annotate > Calling core.questions.show()')
+  const p2 = req
+    .core.questions.show(questionId)
+
+  Promise.all([p1, p2])
+    .then(values => {
+      const [entry, question] = values
+
+      res.render('annotate', {entry, question})
+    })
+    .catch(e => {
+      console.log('ENDPOINT annotate > Failed some request to the core')
+      res.send('failed')
+    })
 }
